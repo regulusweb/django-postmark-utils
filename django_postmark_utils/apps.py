94bb662b2c1b93e73d1ee3878a0024392287c885
Joseph Wayodi <joseph@regulusweb.com>
2018-01-23 16:23:18 +0300

    Fix name of app config

--- django_postmark_utils/apps.py
+++ django_postmark_utils/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
-class DjangoPostmarkUtilConfig(AppConfig):
+class DjangoPostmarkUtilsConfig(AppConfig):
     name = 'django_postmark_utils'
     verbose_name = _("Django Postmark Utils")
 
