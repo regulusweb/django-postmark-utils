85351106b785c5058f8d1a64ea2d08a52f535b46
Joseph Wayodi <joseph@regulusweb.com>
2018-01-23 15:40:24 +0300

    Add verbose name for app config

--- django_postmark_utils/apps.py
+++ django_postmark_utils/apps.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class DjangoPostmarkUtilConfig(AppConfig):
     name = 'django_postmark_utils'
+    verbose_name = _("Django Postmark Utils")
 
     def ready(self):
         from . import signal_handlers
